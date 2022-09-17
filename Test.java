88752095bea58e947d7fd9d1fbc131004dce92ae
Sheeshpal Singh <123sheeshpalsinghsaini@gmail.com>
2022-09-17 17:13:04 +0530

    updated

--- Test.java
+++ Test.java
@@ -1,5 +1,17 @@
 public class Test{
-    public static void main(String[] args) {
+   
+    static int index = -1;
+
+    public static Node createTree(int []nodes){
+        index++;
+        if(nodes[index]==-1)
+            return null;
         
+        Node root = new Node(nodes[index]);
+
+        root.left = createTree(nodes);
+        root.right = createTree(nodes);
+
+        return root;
     }
 }
